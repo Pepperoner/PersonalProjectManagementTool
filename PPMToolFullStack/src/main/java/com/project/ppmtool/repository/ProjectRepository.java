35983393fa3f7f4d7cf17c7cc9ea0c917e382ad1
Pepperoner <boris.lahutin@gmail.com>
2019-01-19 14:38:44 +0200

    create Entity Project

---
+++ PPMToolFullStack/src/main/java/com/project/ppmtool/repository/ProjectRepository.java
@@ -0,0 +1,10 @@
+package com.project.ppmtool.repository;
+
+import com.project.ppmtool.entity.Project;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProjectRepository extends CrudRepository<Project,Long> {
+
+}
