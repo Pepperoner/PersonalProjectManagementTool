04bd39c4162989f75bc3b7280fcf9c7ebe53a730
Pepperoner <boris.lahutin@gmail.com>
2019-01-19 13:35:33 +0200

    first commit

---
+++ PPMToolFullStack/src/main/java/com/project/ppmtool/PpmtoolApplication.java
@@ -0,0 +1,14 @@
+package com.project.ppmtool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PpmtoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PpmtoolApplication.class, args);
+	}
+
+}
+
