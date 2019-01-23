2ee2a85ac159b3b3a8a4d0f6a03f18a52669863c
Pepperoner <boris.lahutin@gmail.com>
2019-01-23 21:59:49 +0200

    added ProjectTask and Backlog Entities

---
+++ PPMToolFullStack/src/main/java/com/project/ppmtool/entity/Backlog.java
@@ -0,0 +1,47 @@
+package com.project.ppmtool.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Backlog {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Integer PTSequence = 0;
+    private String projectIdentifier;
+
+    //OneToOne with project
+
+    //OneToMany with project tasks
+
+    public Backlog() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Integer getPTSequence() {
+        return PTSequence;
+    }
+
+    public void setPTSequence(Integer PTSequence) {
+        this.PTSequence = PTSequence;
+    }
+
+    public String getProjectIdentifier() {
+        return projectIdentifier;
+    }
+
+    public void setProjectIdentifier(String projectIdentifier) {
+        this.projectIdentifier = projectIdentifier;
+    }
+}
