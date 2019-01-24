e04eb50c8ecaa73e9aded1c9c39c30c7f555733f
Pepperoner <boris.lahutin@gmail.com>
2019-01-24 23:59:54 +0200

    added Persist of ProjectTask

---
+++ PPMToolFullStack/src/main/java/com/project/ppmtool/controller/BacklogController.java
@@ -0,0 +1,38 @@
+package com.project.ppmtool.controller;
+
+import com.project.ppmtool.entity.ProjectTask;
+import com.project.ppmtool.service.ProjectTaskService;
+import com.project.ppmtool.service.ValidationErrorService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/api/backlog")
+@CrossOrigin
+public class BacklogController {
+
+    private ProjectTaskService projectTaskService;
+    private ValidationErrorService validationErrorService;
+
+    @Autowired
+    public BacklogController(ProjectTaskService projectTaskService, ValidationErrorService validationErrorService) {
+        this.projectTaskService = projectTaskService;
+        this.validationErrorService = validationErrorService;
+    }
+
+    @PostMapping("/{backlog_id}")
+    public ResponseEntity<?> addPTtoBacklog(@Valid @RequestBody ProjectTask projectTask,
+                                            BindingResult result, @PathVariable String backlog_id){
+        ResponseEntity<?> errorMap = validationErrorService.mapValidationService(result);
+        if (errorMap != null) return errorMap;
+
+        ProjectTask projectTask1 = projectTaskService.addProjectTask(backlog_id, projectTask);
+
+        return new ResponseEntity<ProjectTask>(projectTask1, HttpStatus.CREATED);
+    }
+}
---
+++ PPMToolFullStack/src/main/java/com/project/ppmtool/service/ProjectTaskService.java
@@ -0,0 +1,42 @@
+package com.project.ppmtool.service;
+
+import com.project.ppmtool.entity.Backlog;
+import com.project.ppmtool.entity.ProjectTask;
+import com.project.ppmtool.repository.BacklogRepository;
+import com.project.ppmtool.repository.ProjectTaskRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProjectTaskService {
+
+    private BacklogRepository backlogRepository;
+    private ProjectTaskRepository projectTaskRepository;
+
+    @Autowired
+    public ProjectTaskService(BacklogRepository backlogRepository, ProjectTaskRepository projectTaskRepository) {
+        this.backlogRepository = backlogRepository;
+        this.projectTaskRepository = projectTaskRepository;
+    }
+
+    public ProjectTask addProjectTask(String projectIdentifier, ProjectTask projectTask){
+        Backlog backlog = backlogRepository.findByProjectIdentifier(projectIdentifier);
+        projectTask.setBacklog(backlog);
+
+        Integer backlogSequence = backlog.getPTSequence();
+        backlogSequence++;
+
+        projectTask.setProjectSequence(projectIdentifier+"-"+backlogSequence);
+        projectTask.setProjectIdentifier(projectIdentifier);
+
+//        if (projectTask.getPriority() == 0 || projectTask.getPriority() == null){
+//            projectTask.setPriority(3);
+//        }
+
+//        if (projectTask.getStatus().equals("") || projectTask.getStatus() == null){
+//            projectTask.setStatus("TO-DO");
+//       }
+
+        return projectTaskRepository.save(projectTask);
+    }
+}
