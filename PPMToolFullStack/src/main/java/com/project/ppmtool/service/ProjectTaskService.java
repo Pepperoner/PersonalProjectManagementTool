58abb14bcb5378c36698896c4e877dd5e221e311
Pepperoner <boris.lahutin@gmail.com>
2019-01-27 18:08:42 +0200

    added Delete project task

--- PPMToolFullStack/src/main/java/com/project/ppmtool/service/ProjectTaskService.java
+++ PPMToolFullStack/src/main/java/com/project/ppmtool/service/ProjectTaskService.java
@@ -10,6 +10,8 @@ import com.project.ppmtool.repository.ProjectTaskRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class ProjectTaskService {
 
@@ -81,8 +83,19 @@ public class ProjectTaskService {
     }
 
     public ProjectTask updateByProjectSequence(ProjectTask updatedTask, String backlog_id, String pt_id){
-        ProjectTask projectTask = projectTaskRepository.findByProjectSequence(pt_id);
+
+        ProjectTask projectTask = findPTByProjectSequence(backlog_id,pt_id);
         projectTask = updatedTask;
+
         return projectTaskRepository.save(projectTask);
+    }
+
+    public void deletePTByProjectSequence(String backlog_id, String pt_id){
+        ProjectTask projectTask = findPTByProjectSequence(backlog_id,pt_id);
+        Backlog backlog = projectTask.getBacklog();
+        List<ProjectTask> pts = backlog.getProjectTasks();
+        pts.remove(projectTask);
+        backlogRepository.save(backlog);
+        projectTaskRepository.delete(projectTask);
     }
 }
