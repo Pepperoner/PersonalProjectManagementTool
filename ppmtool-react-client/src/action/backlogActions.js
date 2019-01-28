8cbedb368b171473a872c8d7de9c585a78de51e0
Pepperoner <boris.lahutin@gmail.com>
2019-01-28 20:32:55 +0200

    added validation of Project on ProjectBoard

--- ppmtool-react-client/src/action/backlogActions.js
+++ ppmtool-react-client/src/action/backlogActions.js
@@ -25,7 +25,10 @@ export const getBacklog = backlog_id => async dispatch => {
       type: GET_BACKLOG,
       payload: res.data
     });
-  } catch (error) {
-
+  } catch (err) {
+    dispatch({
+      type: GET_ERRORS,
+      payload: err.response.data
+    });
   }
 }
