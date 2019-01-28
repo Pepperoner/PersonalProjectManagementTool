2f59f458967441601047bbcf427b7fba6081f48a
Pepperoner <boris.lahutin@gmail.com>
2019-01-28 18:40:27 +0200

    Load ProjectTasks to Redux state

--- ppmtool-react-client/src/component/ProjectBoard/ProjectBoard.js
+++ ppmtool-react-client/src/component/ProjectBoard/ProjectBoard.js
@@ -1,8 +1,17 @@
 import React, { Component } from 'react';
 import { Link } from "react-router-dom";
 import Backlog from './Backlog';
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+import { getBacklog } from "../../action/backlogActions";
 
 class ProjectBoard extends Component {
+
+  componentDidMount() {
+    const { id } = this.props.match.params;
+    this.props.getBacklog(id);
+  }
+
   render() {
     const { id } = this.props.match.params;
     return (
@@ -18,4 +27,13 @@ class ProjectBoard extends Component {
   }
 }
 
-export default ProjectBoard;
+ProjectBoard.propTypes = {
+  backlog: PropTypes.object.isRequired,
+  getBacklog: PropTypes.func.isRequired
+};
+
+const mapStateToProps = state => ({
+  backlog: state.backlog
+});
+
+export default connect(mapStateToProps, { getBacklog })(ProjectBoard);
