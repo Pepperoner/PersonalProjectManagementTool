af197834b7b02a7911b0173455233fb1c3405d80
Pepperoner <boris.lahutin@gmail.com>
2019-01-29 15:47:24 +0200

    added error validation on UpdateProjectTask

--- ppmtool-react-client/src/action/backlogActions.js
+++ ppmtool-react-client/src/action/backlogActions.js
@@ -44,3 +44,19 @@ export const getProjectTask = (backlog_id, pt_id, history) => async dispatch => 
     history.push("/dashboard");
   }
 };
+
+export const updateProjectTask = (backlog_id, pt_id, project_task, history) => async dispatch => {
+  try {
+    await axios.patch(`/api/backlog/${backlog_id}/${pt_id}`, project_task)
+    history.push(`/projectBoard/${backlog_id}`)
+    dispatch({
+      type: GET_ERRORS,
+      payload: {}
+    })
+  } catch (err) {
+    dispatch({
+      type: GET_ERRORS,
+      payload: err.response.data
+    })
+  }
+}
