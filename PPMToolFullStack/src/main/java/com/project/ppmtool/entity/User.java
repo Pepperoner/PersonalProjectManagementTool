0b373499d52b04bab47c1c41cec659e96a6123ff
Pepperoner <boris.lahutin@gmail.com>
2019-01-31 14:29:32 +0200

    created User Entity, Service, Repository

--- PPMToolFullStack/src/main/java/com/project/ppmtool/entity/User.java
+++ PPMToolFullStack/src/main/java/com/project/ppmtool/entity/User.java
@@ -1,4 +1,98 @@
 package com.project.ppmtool.entity;
 
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import java.util.Date;
+
+@Entity
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Email(message = "Username must to be an email")
+    @NotBlank(message = "Username is required")
+    @Column(unique = true)
+    private String userName;
+    @NotBlank(message = "Please enter your full name")
+    private String fullName;
+    @NotBlank(message = "Password is required")
+    private String password;
+    @Transient
+    private String confirmPassword;
+    private Date created_At;
+    private Date updated_At;
+
+    //OneToMany with Project
+
+    public User() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getConfirmPassword() {
+        return confirmPassword;
+    }
+
+    public void setConfirmPassword(String confirmPassword) {
+        this.confirmPassword = confirmPassword;
+    }
+
+    public Date getCreated_At() {
+        return created_At;
+    }
+
+    public void setCreated_At(Date created_At) {
+        this.created_At = created_At;
+    }
+
+    public Date getUpdated_At() {
+        return updated_At;
+    }
+
+    public void setUpdated_At(Date updated_At) {
+        this.updated_At = updated_At;
+    }
+
+    @PrePersist
+    protected void onCreate(){
+        this.created_At = new Date();
+    }
+
+    @PreUpdate
+    protected void onUpdate(){
+        this.updated_At = new Date();
+    }
+
 }
---
+++ PPMToolFullStack/src/main/java/com/project/ppmtool/service/UserService.java
@@ -0,0 +1,16 @@
+package com.project.ppmtool.service;
+
+import com.project.ppmtool.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    private UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+}
